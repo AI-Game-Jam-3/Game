bf7a318d4501fbe12da99d94dd9ec2f9a22ede84
olleh-dlrow <1136246408@qq.com>
2023-07-08 15:53:45 +0800

    支持非中心坐标转换

--- Development/client/AIGJ2023/Assets/Scprits/MapManager.cs
+++ Development/client/AIGJ2023/Assets/Scprits/MapManager.cs
@@ -42,14 +42,26 @@ public class MapManager : MonoBehaviour
     {
         // (0, 0) => (SIZE/2, -SIZE/2)
         // y轴相反
-        return new Vector3(coord.x * CELL_SIZE + CELL_SIZE / 2, -coord.y * CELL_SIZE - CELL_SIZE / 2);
+        return new Vector3(coord.x * CELL_SIZE + CELL_SIZE / 2.0f, -coord.y * CELL_SIZE - CELL_SIZE / 2.0f);
+    }
+
+    Vector2 _Pos2Coord(Vector3 pos)
+    {
+        // (SIZE/2, -SIZE/2) => (0, 0)
+        // y轴相反
+        return new Vector2((pos.x - CELL_SIZE / 2.0f) / CELL_SIZE, -(pos.y + CELL_SIZE / 2.0f) / CELL_SIZE);
     }
 
     public Vector2 Pos2Coord(Vector3 pos)
     {
-        // (SIZE/2, -SIZE/2) => (0, 0)
-        // y轴相反
-        return new Vector2((pos.x - CELL_SIZE / 2) / CELL_SIZE, -(pos.y + CELL_SIZE / 2) / CELL_SIZE);
+        var centerPos = new Vector2(0.5f + (int)(pos.x / CELL_SIZE) * CELL_SIZE, -0.5f + (int)(pos.y / CELL_SIZE) * CELL_SIZE);
+        return _Pos2Coord(centerPos);
+    }
+
+    [Button("t")]
+    public void Test()
+    {
+        Debug.Log(Pos2Coord(new Vector3(1.7f, -1.3f, 0)));
     }
 
     public Vector3 GetMapUnitPos(Vector2 coord)
@@ -83,6 +95,7 @@ public class MapManager : MonoBehaviour
         return mapUnitsDict;
     }
 
+    [Button("读取地图")]
     public void ReadMap()
     {
         currentMap.Width = 0;
@@ -178,91 +191,91 @@ public class MapManager : MonoBehaviour
 
 }
 
-#if UNITY_EDITOR
+// #if UNITY_EDITOR
 
-[CustomEditor(typeof(MapManager))]
-public class MapManagerEditor : Editor
-{
-    public override void OnInspectorGUI()
-    {
-        base.OnInspectorGUI();
+// [CustomEditor(typeof(MapManager))]
+// public class MapManagerEditor : Editor
+// {
+//     // public override void OnInspectorGUI()
+//     // {
+//     //     base.OnInspectorGUI();
 
-        var mapManager = target as MapManager;
+//     //     var mapManager = target as MapManager;
 
-        if (GUILayout.Button("读取地图"))
-        {
-            mapManager.ReadMap();
-        }
-    }
+//     //     if (GUILayout.Button("读取地图"))
+//     //     {
+//     //         mapManager.ReadMap();
+//     //     }
+//     // }
 
-    [System.Obsolete("别用")]
-    void ReadMap()
-    {
-        var mapManager = target as MapManager;
-        var currentMap = serializedObject.FindProperty("currentMap").objectReferenceValue as RectMap;
-        serializedObject.Update();
+//     [System.Obsolete("别用")]
+//     void ReadMap()
+//     {
+//         var mapManager = target as MapManager;
+//         var currentMap = serializedObject.FindProperty("currentMap").objectReferenceValue as RectMap;
+//         serializedObject.Update();
 
-        currentMap.Width = 0;
-        currentMap.Height = 0;
-        var gameRoot = currentMap.GetComponent<Transform>().gameObject;
+//         currentMap.Width = 0;
+//         currentMap.Height = 0;
+//         var gameRoot = currentMap.GetComponent<Transform>().gameObject;
 
-        // 获取所有子物体
-        var kinds = new List<GameObject>();
-        foreach (Transform child in gameRoot.transform)
-        {
-            kinds.Add(child.gameObject);
-        }
+//         // 获取所有子物体
+//         var kinds = new List<GameObject>();
+//         foreach (Transform child in gameRoot.transform)
+//         {
+//             kinds.Add(child.gameObject);
+//         }
 
-        Dictionary<string, List<GameObject>> mapUnitsDict = new Dictionary<string, List<GameObject>>();
-        // 遍历所有类型
-        foreach (GameObject kind in kinds)
-        {
-            var typeName = kind.name;
-            var mapUnits = new List<GameObject>();
-            foreach (Transform child in kind.transform)
-            {
-                mapUnits.Add(child.gameObject);
-            }
+//         Dictionary<string, List<GameObject>> mapUnitsDict = new Dictionary<string, List<GameObject>>();
+//         // 遍历所有类型
+//         foreach (GameObject kind in kinds)
+//         {
+//             var typeName = kind.name;
+//             var mapUnits = new List<GameObject>();
+//             foreach (Transform child in kind.transform)
+//             {
+//                 mapUnits.Add(child.gameObject);
+//             }
 
-            mapUnitsDict.Add(typeName, mapUnits);
-        }
+//             mapUnitsDict.Add(typeName, mapUnits);
+//         }
 
-        foreach (var name in mapUnitsDict.Keys)
-        {
-            var mapUnits = mapUnitsDict[name];
+//         foreach (var name in mapUnitsDict.Keys)
+//         {
+//             var mapUnits = mapUnitsDict[name];
 
-            // 遍历所有子物体
-            foreach (GameObject mapUnit in mapUnits)
-            {
-                var coord = mapManager.Pos2Coord(mapUnit.transform.position);
-                var x = (int)coord.x;
-                var y = (int)coord.y;
-                // 取最大的x作为宽度, 取最大的y作为高度
-                currentMap.Width = Mathf.Max(currentMap.Width, x + 1);
-                currentMap.Height = Mathf.Max(currentMap.Height, y + 1);
-            }
-        }
+//             // 遍历所有子物体
+//             foreach (GameObject mapUnit in mapUnits)
+//             {
+//                 var coord = mapManager.Pos2Coord(mapUnit.transform.position);
+//                 var x = (int)coord.x;
+//                 var y = (int)coord.y;
+//                 // 取最大的x作为宽度, 取最大的y作为高度
+//                 currentMap.Width = Mathf.Max(currentMap.Width, x + 1);
+//                 currentMap.Height = Mathf.Max(currentMap.Height, y + 1);
+//             }
+//         }
 
-        currentMap.MapUnits = new MapUnit[currentMap.Height, currentMap.Width];
+//         currentMap.MapUnits = new MapUnit[currentMap.Height, currentMap.Width];
 
-        foreach (var name in mapUnitsDict.Keys)
-        {
-            var mapUnits = mapUnitsDict[name];
+//         foreach (var name in mapUnitsDict.Keys)
+//         {
+//             var mapUnits = mapUnitsDict[name];
 
-            foreach (GameObject mapUnit in mapUnits)
-            {
-                var coord = mapManager.Pos2Coord(mapUnit.transform.position);
-                var x = (int)coord.x;
-                var y = (int)coord.y;
-                Debug.Assert(currentMap.Width > x && currentMap.Height > y, "地图越界");
-                currentMap.MapUnits[y, x] = mapUnit.GetComponent<MapUnit>();
-                currentMap.MapUnits[y, x].UnitType = name;
-            }
-        }
+//             foreach (GameObject mapUnit in mapUnits)
+//             {
+//                 var coord = mapManager.Pos2Coord(mapUnit.transform.position);
+//                 var x = (int)coord.x;
+//                 var y = (int)coord.y;
+//                 Debug.Assert(currentMap.Width > x && currentMap.Height > y, "地图越界");
+//                 currentMap.MapUnits[y, x] = mapUnit.GetComponent<MapUnit>();
+//                 currentMap.MapUnits[y, x].UnitType = name;
+//             }
+//         }
 
-        serializedObject.ApplyModifiedProperties();
-    }
-}
+//         serializedObject.ApplyModifiedProperties();
+//     }
+// }
 
 
-#endif
+// #endif
