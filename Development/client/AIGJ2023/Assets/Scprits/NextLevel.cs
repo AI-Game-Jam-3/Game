421ed0a3431f06aaf6396c07360deb030a9a0711
Jiayi Chen <240829356@qq.com>
2023-07-09 13:05:01 +0800

    Add Level Animation

--- Development/client/AIGJ2023/Assets/Scprits/NextLevel.cs
+++ Development/client/AIGJ2023/Assets/Scprits/NextLevel.cs
@@ -4,23 +4,26 @@ using UnityEngine;
 
 public class NextLevel : MonoBehaviour
 {
+    LevelAnimation levelAnimation;
+
     public int nextLevelIndex = 1;
     // Start is called before the first frame update
     void Start()
     {
-
+        levelAnimation = GetComponent<LevelAnimation>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.TryGetComponent<SmallTrigger>(out var comp))
         {
-            GameManager.Instance?.EnterLevel(nextLevelIndex);
+            levelAnimation.TriggerQuitAnimation();
+            levelAnimation.QuitAnimationFinish += () => 
+            { GameManager.Instance?.EnterLevel(nextLevelIndex); };
         }
     }
 }
